b2fc7b862c5604a38e3be49e94d192fe5b503d91
Brito <jmbmaluno@gmail.com>
2024-09-05 14:49:57 -0300

    começando inclusao do zero

--- main.cpp
+++ main.cpp
@@ -110,16 +110,4 @@ int main(){
     Mao m;
 
     m.imprimir();
-
-    m.incluir(3,log(m.size()));
-
-    m.imprimir();
-
-    m.teste();
-
-    m.imprimir();
-
-    m.incluir(4, log(m.size()));
-
-    m.imprimir();
 }
